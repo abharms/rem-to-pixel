7316b711c76b3ae034d7b76656969147bb972b1e
aharms24 <aharms@works24.com>
2025-02-06 13:04:14 -0600

    Add defer to script

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,8 @@
 module.exports = function (eleventyConfig) {
 	return {
 		dir: {
-			input: 'content',
-			output: 'dist/rem-to-pixel/browser', // Output directly to browser folder
-			layouts: '_layouts'
-		},
-		permalinks: {
-			structure: ':title'
+			input: 'content/blog',
+			output: 'dist/rem-to-pixel/browser'
 		}
 	};
 };
