8f7872688ddce49c7493917f5093b761ab631a67
aharms24 <aharms@works24.com>
2025-02-10 16:14:57 -0600

    Update yml

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ module.exports = function (eleventyConfig) {
 	return {
 		dir: {
 			input: 'src/blog', // Location of your markdown files
-			output: 'dist/blog' // Output folder for the generated HTML
-		}
+			output: 'dist/rem-to-pixel/browser/blog' // Output to the correct directory for Netlify
+		},
+		passthroughFileCopy: true
 	};
 };
